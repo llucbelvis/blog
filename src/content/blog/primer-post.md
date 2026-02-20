50a9c9fc98eba626b0eca6eb320a8ef1e80a9d1f
Alejandro <alexdlc470@gmail.com>
2026-02-20 20:39:08 +0100

    Estructura de blog creada

---
+++ src/content/blog/primer-post.md
@@ -0,0 +1,9 @@
+---
+title: "Mi primera entrada"
+date: 2026-02-20
+description: "Hola, este es el blog de Lluc hecho con Astro."
+---
+
+# ¡Bienvenidos a mi blog!
+
+Este es el contenido del artículo. Aquí Lluc puede escribir todo lo que quiera.
---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+  }),
+});
+
+export const collections = { blog };
